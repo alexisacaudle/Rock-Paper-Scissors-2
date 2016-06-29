9559a77f91cc6690199515e93d3d02861a1ce15c
cauda964 <cauda964@newschool.edu>
2016-06-28 20:18:46 -0400

    data segues, unstable segue

--- RPC-2/ViewController.swift
+++ RPC-2/ViewController.swift
@@ -11,51 +11,80 @@ import UIKit
 let computerChoiceArray = ["rock", "paper", "scissors"]
 let computerChoiceArrayNumber = Int(arc4random_uniform(UInt32(computerChoiceArray.count)))
 var computerChoiceString = String()
+var userChoice = String()
+var results = String()
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBAction func rockButton(sender: AnyObject) {
+        userChoice = "rock"
+        assignArrayString()
     }
-
+    
+    @IBAction func paperButton(sender: AnyObject) {
+        userChoice = "paper"
+        assignArrayString()
+    }
+    @IBAction func scissorsButton(sender: AnyObject) {
+        userChoice = "scissors"
+        assignArrayString()
+    }
+    
     func assignArrayString() {
         if (computerChoiceArrayNumber == 0) {
             computerChoiceString = "rock"
+            if (userChoice == "rock"){
+                results = "It's a tie"
+                print("It's a tie")
+            }else if(userChoice == "paper"){
+                results = "You win!"
+                print("You win!")
+            }else{
+                results = "You win!"
+                print("Computer wins")
+            }
+            print("Computer:", computerChoiceString)
         }else if (computerChoiceArrayNumber == 1){
             computerChoiceString = "paper"
+            if (userChoice == "rock"){
+                results = "Computer wins"
+                print("Computer wins")
+            }else if(userChoice == "paper"){
+                results = "It's a tie"
+                print("It's a tie")
+            }else{
+                results = "You win!"
+                print("You win!")
+            }
+            print("Computer:", computerChoiceString)
         }else{
             computerChoiceString = "scissors"
-        }
-        print("Computer:", computerChoiceString)
-    }
-    
-    @IBAction func rockButton(sender: AnyObject) {
-        assignArrayString()
-        if (computerChoiceString == "rock"){
-            print("It's a tie!")
-            func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-                let resultsVC : resultsScreen = segue.destinationViewController as! resultsScreen
-                resultsVC.results = "It's a tie!"
+            if (userChoice == "rock"){
+                results = "You win!"
+                print("You win!")
+            }else if(userChoice == "paper"){
+                results = "Computer wins"
+                print("Computer wins")
+            }else{
+                results = "It's a tie"
+                print("It's a tie")
             }
-        }else if (computerChoiceString == "paper"){
-            print("Computer wins ☹️")
-            func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-                let resultsVC : resultsScreen = segue.destinationViewController as! resultsScreen
-                resultsVC.results = "Computer wins ☹️"
-            }
-        }else{
-            print("You win! 😀🎉")
-            func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-                let resultsVC : resultsScreen = segue.destinationViewController as! resultsScreen
-                resultsVC.results = "You win! 😀🎉"
-            }
+            print("Computer:", computerChoiceString)
         }
     }
-    @IBAction func paperButton(sender: AnyObject) {
-    }
-    @IBAction func scissorsButton(sender: AnyObject) {
+    
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        let resultsVC : resultsScreen = segue.destinationViewController as! resultsScreen
+        resultsVC.sentResultsString = results
+        resultsVC.computerChoiceString = computerChoiceString
     }
     
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -66,7 +95,8 @@ class ViewController: UIViewController {
 //MARK : resultsScreen class
 class resultsScreen: UIViewController{
     
-    var results = String()
+    var sentResultsString = String()
+    var computerChoiceString = String()
     
     @IBOutlet weak var computerResultsLabel: UILabel!
     
@@ -75,13 +105,12 @@ class resultsScreen: UIViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        gameResultsLabel.text = results
-        
+        gameResultsLabel.text = sentResultsString
+        computerResultsLabel.text = "The computer chose \(computerChoiceString)!"
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
 }
